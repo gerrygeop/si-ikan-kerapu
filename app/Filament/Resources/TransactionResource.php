f0a8bd3bfc8f4709c0e3460a748eaa7c39ac0460
gerrygeop <gerryja28@gmail.com>
2025-08-04 02:53:25 +0800

    feat: add view page & filter table by entry datetime

--- app/Filament/Resources/TransactionResource.php
+++ app/Filament/Resources/TransactionResource.php
@@ -5,8 +5,13 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\TransactionResource\Pages;
 use App\Filament\Resources\TransactionResource\RelationManagers;
 use App\Models\Transaction;
+use Carbon\Carbon;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Infolists;
+use Filament\Infolists\Infolist;
+use Filament\Pages\SubNavigationPosition;
+use Filament\Resources\Pages\Page;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
@@ -19,6 +24,9 @@ class TransactionResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
     protected static ?string $label = 'Transaksi';
+    protected static ?int $navigationSort = 1;
+
+    protected static SubNavigationPosition $subNavigationPosition = SubNavigationPosition::Top;
 
     public static function form(Form $form): Form
     {
@@ -128,7 +136,7 @@ class TransactionResource extends Resource
                             ->required(),
 
                         Forms\Components\Textarea::make('notes')
-                            ->label('Keterangan')
+                            ->label('Catatan')
                             ->columnSpanFull(),
                     ])
                     ->columns(2)
@@ -189,9 +197,38 @@ class TransactionResource extends Resource
                     ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
-                //
+                Tables\Filters\Filter::make('entry_datetime')
+                    ->label('Tanggal masuk')
+                    ->form([
+                        Forms\Components\DatePicker::make('dari_tanggal')
+                            ->placeholder(fn($state): string => 'Jan 01, ' . now()->subYear()->format('Y')),
+                        Forms\Components\DatePicker::make('sampai_tanggal')
+                            ->placeholder(fn($state): string => now()->format('M d Y')),
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        return $query
+                            ->when(
+                                $data['dari_tanggal'] ?? null,
+                                fn(Builder $query, $date): Builder => $query->whereDate('entry_datetime', '>=', $date),
+                            )
+                            ->when(
+                                $data['sampai_tanggal'] ?? null,
+                                fn(Builder $query, $date): Builder => $query->whereDate('entry_datetime', '<=', $date),
+                            );
+                    })
+                    ->indicateUsing(function (array $data): array {
+                        $indicators = [];
+                        if ($data['dari_tanggal'] ?? null) {
+                            $indicators['dari_tanggal'] = 'Dari Tanggal ' . Carbon::parse($data['dari_tanggal'])->toFormattedDateString();
+                        }
+                        if ($data['sampai_tanggal'] ?? null) {
+                            $indicators['sampai_tanggal'] = 'Sampai Tanggal ' . Carbon::parse($data['sampai_tanggal'])->toFormattedDateString();
+                        }
+                        return $indicators;
+                    }),
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
@@ -208,6 +245,82 @@ class TransactionResource extends Resource
             });
     }
 
+    public static function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+
+                Infolists\Components\Section::make('Transaksi')
+                    ->schema([
+                        Infolists\Components\TextEntry::make('origin')
+                            ->label('Sumber ikan'),
+                        Infolists\Components\TextEntry::make('quantity')
+                            ->label('Jumlah (Kg)')
+                            ->suffix(' Kg'),
+                        Infolists\Components\TextEntry::make('price_per_unit')
+                            ->label('Harga per Kg')
+                            ->money('IDR', locale: 'id')
+                            ->badge(),
+                        Infolists\Components\TextEntry::make('total_price')
+                            ->label('Total Harga')
+                            ->money('IDR', locale: 'id')
+                            ->badge(),
+                        Infolists\Components\TextEntry::make('entry_datetime')
+                            ->label('Tanggal Masuk')
+                            ->date()
+                            ->icon('heroicon-o-calendar-days'),
+                        Infolists\Components\TextEntry::make('entry_datetime')
+                            ->label('Waktu Masuk')
+                            ->time()
+                            ->icon('heroicon-o-clock'),
+                        Infolists\Components\TextEntry::make('user.name')
+                            ->label('Diinput Oleh'),
+                        Infolists\Components\Fieldset::make('Catatan')
+                            ->schema([
+                                Infolists\Components\TextEntry::make('notes')
+                                    ->label('')
+                                    ->default('-')
+                                    ->columnSpanFull(),
+                            ])
+                    ])
+                    ->collapsible()
+                    ->columns(2)
+                    ->columnSpan(2),
+
+                Infolists\Components\Group::make()
+                    ->schema([
+                        Infolists\Components\Section::make('Jenis Ikan')
+                            ->schema([
+                                Infolists\Components\TextEntry::make('fish.name')->label('Jenis Ikan'),
+                                Infolists\Components\TextEntry::make('fish.description')
+                                    ->label('Keterangan')
+                                    ->columnSpanFull(),
+                            ])
+                            ->collapsible(),
+
+                        Infolists\Components\Section::make('Pemasok')
+                            ->schema([
+                                Infolists\Components\TextEntry::make('supplier.name')
+                                    ->label('Nama Pemasok'),
+                                Infolists\Components\TextEntry::make('supplier.phone_number')
+                                    ->label('No. Telp'),
+                                Infolists\Components\TextEntry::make('supplier.address')
+                                    ->label('Alamat')
+                                    ->columnSpanFull(),
+                            ])
+                            ->collapsible()
+                            ->columns(2),
+
+                        Infolists\Components\Section::make([
+                            Infolists\Components\TextEntry::make('created_at'),
+                            Infolists\Components\TextEntry::make('updated_at'),
+                        ]),
+                    ])
+                    ->columnSpan(1),
+            ])
+            ->columns(3);
+    }
+
     public static function getRelations(): array
     {
         return [
@@ -215,11 +328,20 @@ class TransactionResource extends Resource
         ];
     }
 
+    public static function getRecordSubNavigation(Page $page): array
+    {
+        return $page->generateNavigationItems([
+            Pages\ViewTransaction::class,
+            Pages\EditTransaction::class,
+        ]);
+    }
+
     public static function getPages(): array
     {
         return [
             'index' => Pages\ListTransactions::route('/'),
             'create' => Pages\CreateTransaction::route('/create'),
+            'view' => Pages\ViewTransaction::route('/{record}'),
             'edit' => Pages\EditTransaction::route('/{record}/edit'),
         ];
     }
