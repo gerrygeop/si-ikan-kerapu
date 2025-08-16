de1cecae7773710f20d9d209413b2286b2dd7923
gerrygeop <gerryja28@gmail.com>
2025-08-16 21:16:44 +0800

    feat: comment date picker

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -2,8 +2,35 @@
 
 namespace App\Filament\Pages;
 
-class Dashboard extends \Filament\Pages\Dashboard
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Form;
+use Filament\Forms\Get;
+use Filament\Pages\Dashboard as BaseDashboard;
+
+class Dashboard extends BaseDashboard
 {
+    // use BaseDashboard\Concerns\HasFiltersForm;
+
+    // public function filtersForm(Form $form): Form
+    // {
+    //     return $form
+    //         ->schema([
+    //             Section::make()
+    //                 ->schema([
+    //                     DatePicker::make('startDate')
+    //                         ->label('Dari Tanggal')
+    //                         ->maxDate(fn(Get $get) => $get('endDate') ?: now()),
+    //                     DatePicker::make('endDate')
+    //                         ->label('Sampai Tanggal')
+    //                         ->minDate(fn(Get $get) => $get('startDate') ?: now())
+    //                         ->maxDate(now()),
+    //                 ])
+    //                 ->columns(2),
+    //         ]);
+    // }
+
     public function getColumns(): int | string | array
     {
         return 2;
